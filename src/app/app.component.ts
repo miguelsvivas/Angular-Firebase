9dbee7cd2de994d5e163998ff5879a7f533d66c3
Miguel <vivasmiguel96@gmail.com>
2022-05-19 10:55:03 -0500

    Creando la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-firebaseApp';
+
+
+export class AppComponent  implements OnInit{
+
+  config!:any;
+  collection = {count: 60, data: [] as any[]}
+
+
+
+
+ ngOnInit(): void {
+
+  
+
+
+    for (var i = 0; i < this.collection.count; i++) {
+      this.collection.data.push({
+          id:i,
+          nombre: "nombre"+i,
+          apellido: "apellido"+i
+        })
+    }
+
+
+     this.config = {
+       itemsPerPage : 10,
+       currentPage: 1,
+       totalItems: this.collection.count
+     }
+ }
+
+ pageChanged(event:number){
+   this.config.currentPage = event;
+ }
+
+ eliminar(item:any):void{
+   this.collection.data.pop();
+ }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
